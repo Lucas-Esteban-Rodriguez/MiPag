8320ea2e18bfdb0ccf536d2046e35f8e32cf3131
Lucas-Esteban-Rodriguez <eteban2412@gmail.com>
2022-07-04 22:35:39 -0300

    Seccion contacto terminada

--- español-english.js
+++ español-english.js
@@ -12,3 +12,10 @@ $('#Nosotros').click(function () {
     $('.body-contacto').removeClass('show').addClass('slow') && $('#Contacto').removeClass('nav-link__active')
     $('.body-información').removeClass('show').addClass('slow') && $('#Informacion').removeClass('nav-link__active')
 })
+
+$('#Contacto').click(function () {
+    $('.body-inicio').removeClass('show').addClass('slow') && $('#Inicio').removeClass('nav-link__active')
+    $('.body-nosotros').addClass('show').addClass('slow') && $('#Nosotros').removeClass('nav-link__active')
+    $('.body-contacto').removeClass('show').removeClass('slow') && $('#Contacto').addClass('nav-link__active')
+    $('.body-información').removeClass('show').addClass('slow') && $('#Informacion').removeClass('nav-link__active')
+})
