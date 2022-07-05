72f1cb6deaa15a4b070020c8f78212cf80631e45
Lucas-Esteban-Rodriguez <eteban2412@gmail.com>
2022-07-04 22:40:47 -0300

    Bug animacion body-contacto

--- español-english.js
+++ español-english.js
@@ -15,7 +15,7 @@ $('#Nosotros').click(function () {
 
 $('#Contacto').click(function () {
     $('.body-inicio').removeClass('show').addClass('slow') && $('#Inicio').removeClass('nav-link__active')
-    $('.body-nosotros').addClass('show').addClass('slow') && $('#Nosotros').removeClass('nav-link__active')
-    $('.body-contacto').removeClass('show').removeClass('slow') && $('#Contacto').addClass('nav-link__active')
+    $('.body-nosotros').removeClass('show').addClass('slow') && $('#Nosotros').removeClass('nav-link__active')
+    $('.body-contacto').addClass('show').removeClass('slow') && $('#Contacto').addClass('nav-link__active')
     $('.body-información').removeClass('show').addClass('slow') && $('#Informacion').removeClass('nav-link__active')
 })
